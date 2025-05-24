500fc62a1e003478d3c6b284cd2b79fa85cd7413
jenicke123 <cjjenicke32@.gmail.com>
2025-05-24 04:52:16 -0500

    added important comment

--- src/main/java/net/jenicke123/oredrillermod/CompactOreDrilling.java
+++ src/main/java/net/jenicke123/oredrillermod/CompactOreDrilling.java
@@ -5,11 +5,13 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Very important Content
 public class CompactOreDrilling implements ModInitializer {
 	public static final String MOD_ID = "oredrillermod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+
 	}
 }
